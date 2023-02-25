448acdf1f8b437a5c453ef630124def87c4d417c
Hans Reichgelt <hreichgelt06@gmail.com>
2023-02-24 20:48:24 -0600

    get all todos from db

--- src/components/AllTodoCards.js
+++ src/components/AllTodoCards.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BsTrash } from "react-icons/bs"
 import { FaCheck } from "react-icons/fa"
 // Past due = red || Due soon = yellow || due later = green (card change color based on date?)
 
 
 const AllTodos = (props) => {
+    const [todo, setTodo] = useState([]);
+
+    useEffect(() => {
+        const url = "http://localhost:8080/todo";
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(url);
+            const json = await response.json();
+            console.log(json);
+            setTodo(json)
+        } catch (error){
+            console.log("error", error);
+        }
+    };
+
+    fetchData();
+    }, []);
+
     return (
-    <div className="all-todos">
+        <div className="all-todos-container">
+            {todo.map((todo) => {
+                return (
         <div className="todo-container">
-            <div className="todo-card" style={{backgroundColor: 'red'}}>
+            <div className="todo-card" key={todo.id} style={{backgroundColor: 'red'}}>
                 <button className="complete"><FaCheck /></button>
                 <button className="delete"><BsTrash /></button>
-                <div className="details" style={{color: 'black'}}>{props.text}
-                    <h3 className="title">{props.title}</h3>
-                    <h4 className="description">{props.description}</h4>
-                    <h4 className="dueAt">{props.dueAt}</h4>
+
+                <div className="details" style={{color: 'black'}}>{todo.text}
+                    <h3 className="title">{todo.title}</h3>
+                    <h4 className="description">{todo.description}</h4>
+                    <h4 className="dueAt">{todo.dueAt}</h4>
                 </div>
             </div>
-        </div>
-    </div>
+         </div>
+                )
+            })}
+     </div>
     )
 }
+
 export default AllTodos
